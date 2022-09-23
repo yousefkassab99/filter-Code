83666ad098720184ac191dc27dcae2436ebbb2d4
yousefkassab99 <105107425+yousefkassab99@users.noreply.github.com>
2022-09-23 17:02:45 +0300

    Redux-filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// import { Users } from "./users";
+import "./App.css";
+import Table from "./Table";
+import axios from "axios";
+
+//////////////////////BASIC SEARCH
+
+// function App() {
+//   const [query, setQuery] = useState("");
+//   return (
+//     <div className="app">
+//       <input
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value.toLowerCase())}
+//       />
+//       <ul className="list">
+//         {Users.filter((asd) =>
+//           asd.first_name.toLowerCase().includes(query)
+//         ).map((user) => (
+//           <li className="listItem" key={user.id}>
+//             {user.first_name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+///////////////////////SEARCH ON A DATATABLE
+
+// function App() {
+//   const [query, setQuery] = useState("");
+//   const keys = ["first_name", "last_name", "email"];
+//   const search = (data) => {
+//     return data.filter((item) =>
+//       keys.some((key) => item[key].toLowerCase().includes(query))
+//     );
+//   };
+// return (
+//   <div className="app">
+//       <input
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value.toLowerCase())}
+//       />
+//     {<Table data={Search(Users)} />}
+//   </div>
+// );
+// }
+
+
+////////////////////// API SEARCH
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://localhost:5000?q=${query}`);
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 2) fetchData();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <input
+          className="search"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+      {<Table data={data} />}
     </div>
   );
 }
